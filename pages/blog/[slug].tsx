f299b684223d1c36e8182dd762e7b93eaca4515e
jaykhan-dev <jaykhan.sound@gmail.com>
2023-01-09 20:14:04 -0500

    export default in blog slug

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,2 +1,14 @@
 import client from "../../lib/apollo";
 import { gql } from "@apollo/client";
+import { NextPage } from "next";
+
+const BlogPost: NextPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Blog Post</h1>
+      </div>
+    </>
+  );
+};
+export default BlogPost;
