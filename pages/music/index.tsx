ed5b2ad6c023e5dd7f382ceac15f8e7a07d40579
jaykhan-dev <jaykhan.sound@gmail.com>
2023-01-11 00:02:35 -0500

    added rich text

--- pages/music/index.tsx
+++ pages/music/index.tsx
@@ -17,6 +17,7 @@ export async function getStaticProps() {
             id
           }
           track {
+            3+6
             url
             id
           }
--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { NextPage } from "next";
 import { motion } from "framer-motion";
-// import { RichText } from "@graphcms/rich-text-react-renderer";
+import { RichText } from "@graphcms/rich-text-react-renderer";
 
 export async function getStaticProps({ params }: any) {
   const GET_PROJECTS = gql`
@@ -16,6 +16,7 @@ export async function getStaticProps({ params }: any) {
         slug
         content {
           html
+          raw
         }
         projectImage {
           url
@@ -74,13 +75,13 @@ const textVariants = {
   },
 };
 
-const Project: NextPage = ({ project }: any) => {
+const Project: NextPage = ({ project, children }: any) => {
   return (
     <>
       <Head>
         <title>{project.title}</title>
       </Head>
-      <div className="grid place-items-center relative bg-gradient-to-b from-zinc-900/90 to-zinc-900">
+      <div className="grid place-items-center relative bg-gradient-to-b from-black to-zinc-900 p-4">
         <motion.div
           initial="hidden"
           animate="visible"
@@ -102,7 +103,7 @@ const Project: NextPage = ({ project }: any) => {
           className="z-40 lg:w-1/2 mx-auto p-4"
         >
           <h1 className="text-6xl font-black my-4">{project.title}</h1>
-          <p className="text-xl my-8">{project.excerpt}</p>
+          <p className="text-3xl my-8 text-sky-500">{project.excerpt}</p>
           {/* <RichText
             content={project.content.html}
             renderers={{
@@ -111,10 +112,29 @@ const Project: NextPage = ({ project }: any) => {
             }}
           /> */}
           <button className="badge badge-accent">{project.category}</button>
-          <article
+          {/* <article
             className="my-4"
             dangerouslySetInnerHTML={{ __html: project?.content?.html }}
-          ></article>
+          ></article> */}
+
+          {/* RICH TEXT HYGRAPH */}
+          <RichText
+            content={project?.content.raw}
+            renderers={{
+              h2: ({ children }: any) => (
+                <h2 className="text-4xl font-bold mt-4 py-4">{children}</h2>
+              ),
+              h3: ({ children }) => <h3 className="text-2xl">{children}</h3>,
+              p: ({ children }) => (
+                <p className="my-4 leading-loose">{children}</p>
+              ),
+              blockquote: ({ children }) => (
+                <blockquote className="p-4 border-l-4 text-2xl italic">
+                  {children}
+                </blockquote>
+              ),
+            }}
+          />
           {/* <div>
             {project.tags.map((tag: any) => (
               <li>{tag.tag}</li>
